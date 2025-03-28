d0dfebb20d9ee8a501dc0ac6335059056fc2a26f
GitSteve654 <stevan.ilic.work@gmail.com>
2025-03-28 22:10:50 +0100

    tab logic, account info skeleton

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ window.onload = initLoad;
 
 function initLoad(){
     loadLogInLogic();
+    loadHeadLogic();
 }
 
 function fadeIn(query, durration, type, callabck){
@@ -25,4 +26,93 @@ function fadeOut(query, durration, callabck){
         element.onanimationend = null;
         if(callabck) callabck();
     }
+}
+
+/*--Head Logic---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+function loadHeadLogic(){
+    headAccountLogic();
+    headTabLogic();
+}
+function headAccountLogic(){
+    const accountDropDown = document.querySelector(".account-drop-down");
+    const accountAboutMe = document.querySelector("#account-about-me");
+    const accountLogOff = document.querySelector("#account-log-off");
+    const accountIcon = document.querySelector(".account-icon");
+    
+    let eventInterupted = false;
+    let accountOptionsShow = false;
+    accountIcon.onclick = ()=>{
+        if(accountOptionsShow) closeAccountOptions();
+        else{
+            accountOptionsShow = true;
+            accountDropDown.style.animation = "show-account-options ease-in-out 0.2s";
+            accountDropDown.style.display = "block";
+            accountDropDown.onanimationend = ()=>{
+                accountDropDown.style.animation = "none";
+                accountDropDown.onanimationend = null;
+                window.addEventListener("click", closeAccountOptions);
+            }
+        }
+    }
+    accountAboutMe.onclick = ()=>{
+        sendInteruptEvent();
+        console.log("about me");
+    }
+    accountLogOff.onclick = ()=>{
+        sendInteruptEvent();
+        console.log("log off");
+    }
+
+    window.addEventListener("interupt-close-account", ()=>{
+        eventInterupted = true;
+    });
+    function closeAccountOptions(){
+        setTimeout(()=>{
+            if(eventInterupted) eventInterupted = false;
+            else{
+                window.removeEventListener("click", closeAccountOptions);
+                accountOptionsShow = false;
+                accountDropDown.style.animation = "hide-account-options ease-in-out 0.2s";
+                accountDropDown.onanimationend = ()=>{
+                    accountDropDown.style.animation = "none";
+                    accountDropDown.style.display = "none";
+                    accountDropDown.onanimationend = null;
+                }
+            }
+        }, 100);
+    }
+    function sendInteruptEvent(){
+        const interuptEvent = new Event("interupt-close-account");
+        window.dispatchEvent(interuptEvent);
+    }
+}
+function headTabLogic(){
+    const screens = ["dahsboard", "mail", "calendar", "drive", "students", "homework"];
+    const tabs = document.querySelectorAll(".tab");
+
+    let currTab = 0;
+    for(let i = 0; i < tabs.length; i++){
+        tabs[i].onclick = ()=>{
+            if(currTab !== i){
+                const screensToClose = document.querySelectorAll(".tab-affected");
+                for(let j = 0; j < screensToClose.length; j++) screensToClose[j].style.display = "none";
+                document.getElementById(screens[i]).style.display = "block";
+               
+                if(currTab !== -1){
+                    const currSelectedTab = document.querySelector(".selected-tab");
+                    currSelectedTab.classList.remove("selected-tab");
+                }
+                tabs[i].classList.add("selected-tab");
+                currTab = i;
+            }
+        }
+    }
+    window.addEventListener("close-all-tabs", ()=>{
+        const screensToClose = document.querySelectorAll(".tab-affected");
+        for(let j = 0; j < screensToClose.length; j++) screensToClose[j].style.display = "none";
+        currTab = -1;
+
+        const currSelectedTab = document.querySelector(".selected-tab");
+        currSelectedTab.classList.remove("selected-tab");
+    });
 }
